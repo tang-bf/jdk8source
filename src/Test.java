7554d7126fb0e4e0c48a3d4e6ca7fedb8bf32525
tang-bf <1535710733@qq.com>
2021-01-21 19:18:11 +0800

    针对jdk8一些源码加深理解学习

---
+++ src/Test.java
@@ -0,0 +1,21 @@
+import java.util.HashMap;
+
+/**
+ * @ProjectName: jdk8source
+ * @Package: PACKAGE_NAME
+ * @ClassName: Test
+ * @Description:
+ * @Author: tbf
+ * @CreateDate: 2021-01-20 22:16
+ * @UpdateUser: Administrator
+ * @UpdateDate: 2021-01-20 22:16
+ * @UpdateRemark:
+ * @Version: 1.0
+ */
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(">>>>>>>>>");
+        HashMap<String,String> hashMap1 = new HashMap<String, String>(15,0.75f);
+    }
+}
